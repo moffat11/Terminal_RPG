1e54e013cbb1b9ef925f3b434ac2abc81597f1a4
Moffat Muriithi <moff@Moffats-Air.lan>
2026-03-05 22:36:47 -0700

    Day 1: Implemented raw pointers and memory address mutation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
 int main(){
-    // std::cout is how C++ prints to the terminal
-    // std::endl adds a line break at the end
-    std::cout << "Hello World!" << std::endl;
+    // 1. Create a normal variable
+    int playerHealth = 100;
+    // 2. Create a pointer that stores the physical memory address of playerHealth
+    // The '&' symbol means "get the memory address of"
+    int* healthPointer = &playerHealth;
 
-    // Returning 0 tells your Mac's opertaing system that the program ran without crashing
+    // Print them out to see the difference
+    std::cout << "Player Health Value: " << playerHealth << std::endl;
+
+    //This prints a hexadecimal number, the actual physical memory location which I sometimes 
+    // visualize as a box, in the RAM
+    std::cout << "Memory Address in RAM: " << healthPointer << std::endl;
+
+    // 3. The dereference operator ('*')
+    // Putting a '*' in front of a pointer means "go to that address and get the value"
+    std::cout << "Value at the Address: " << *healthPointer << std::endl;
+    
+    // 4. Changing the value directly through memory
+    *healthPointer = 80;
+
+    std::cout << "New Player Health: " << playerHealth << std::endl;
+
     return 0;
 }
