a6a3f19df8ff53e46bf653f77d297f5a80fac027
Moffat Muriithi <moff@Moffats-Air.lan>
2026-03-05 21:30:53 -0700

    Day 1: Hello World

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    // std::cout is how C++ prints to the terminal
+    // std::endl adds a line break at the end
+    std::cout << "Hello World!" << std::endl;
+
+    // Returning 0 tells your Mac's opertaing system that the program ran without crashing
+    return 0;
+}
