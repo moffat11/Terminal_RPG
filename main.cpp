70d2d2526b74bced69b24efe29484f29c3f4be32
Moffat Muriithi <moff@Moffats-MacBook-Air.local>
2026-03-12 12:11:21 -0600

    Integrated Mersenne Twister RNG for dynamic damage and critical strikes

--- main.cpp
+++ main.cpp
@@ -3,13 +3,30 @@
 #include "Player.h" // Include our new Player class!
 // Include the new Enemy
 #include "Enemy.h"
-// Gonna rewrite the main.cpp file and do the game loop
+// Including the <random> library in order to initialize our Random Number Generator before the main
+// loop starts.
+#include <random>
+// Going rewrite the main.cpp file and do the game loop
 
 int main() {
     std::cout << "--- THE DUNGEON GATES OPEN ---" << std::endl;
 
     std::unique_ptr<Player> hero = std::make_unique<Player>("MW", 100, 50);
     std::unique_ptr<Enemy> goblin = std::make_unique<Enemy>("G.Goblin", 100);
+
+    // --- Random Number Generator ---
+    // Obtains a random seed from the Mac's hardware
+    std::random_device rd;
+    // Seeds the Mersenne Twister engine
+    std::mt19937 gen(rd());
+
+    //Defined Ranges
+    // Player hits between 15 - 35
+    std::uniform_int_distribution<> playerDmgRange(15, 35);
+    // Goblin hits between 10 - 25
+    std::uniform_int_distribution<> enemyDmgRange(10, 25);
+    // Roll a d100 for percentages
+    std::uniform_int_distribution<> critChance(1, 100);
 
     int round = 1;
 
@@ -25,8 +42,19 @@ int main() {
         std::cin >> choice; // The engine pauses her waiting for your keyboard
         
         if (choice == 1) {
-            std::cout << "\n*** You swing your sword ***" << std::endl;
-            goblin->takeDamage(30);
+            //Roll for base damage
+            int damageDealt = playerDmgRange(gen);
+
+            // Roll for Critical Strike (15% Chance)
+            if (critChance(gen) <= 15){
+                // Double the damage
+                damageDealt *= 2;
+                std::cout << "\n*** CRITICAL STRIKE! ***" << std::endl;
+            }
+            else {
+                std::cout << "\n*** You swing your sword! ***" << std::endl;
+            }
+            goblin->takeDamage(damageDealt);
         }
         else if (choice == 2) {
             hero->heal();
@@ -39,7 +67,9 @@ int main() {
         if (goblin->isAlive()) {
             std::cout << "\n*** Enemy Turn ***" << std::endl;
             goblin->decideAction();
-            hero->takeDamage(20); //The Goblin  hits back
+            // The Goblin rolls for its damage
+            int goblinDamage = enemyDmgRange(gen);
+            hero->takeDamage(goblinDamage); //The Goblin  hits back
         }
 
         round++;
